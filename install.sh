9b50e0ae0a262e4fb11c52c4fec186534a5265ba
Andy <90059029+AndyPra@users.noreply.github.com>
2023-01-26 14:07:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ set -e
 
 export GITHUB_SOURCE="v0.12.2"
 export SCRIPT_RELEASE="v0.12.2"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/AndyPra/pteroandy-installer/master/install.sh"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/AndyPra/pteroandy-installer/pterodactyl-installer"
 
 LOG_PATH="/var/log/pteroandy-installer.log"
 
